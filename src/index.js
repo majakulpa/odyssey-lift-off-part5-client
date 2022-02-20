ff8c4c4ca0398acb133b0c278b8b459455cfb381
Maja Kulpa-Malecka <majaweronikakulpa@gmail.com>
2022-02-20 16:57:19 +1100

    change server endpoint from dev to prod

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
+  uri: "https://liftoff-catstronauts.herokuapp.com/",
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
@@ -14,5 +14,5 @@ ReactDOM.render(
     <GlobalStyles />
     <Pages />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
